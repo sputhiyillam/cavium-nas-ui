527a50606b0bf9b7314a63226db3a12946337a3f
bharathwaaj <bharathwaaj.s@gmail.com>
2012-12-27 18:26:50 +0530

    Work in progress.

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+require.config({
+    paths: {
+        jquery: 'libs/jquery/jquery',
+        underscore: 'libs/underscore/underscore',
+        backbone: 'libs/backbone/backbone'
+    }
+});
+
+require(['app'], function(App) {
+    App.initialize();
+});
---
+++ assets/js/router.js
@@ -0,0 +1,13 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/disks/default',
+  'views/volumes/default',
+  'views/shares/default',
+  'views/usergroups/default',
+  'views/backup/default',
+  'views/settings/default',
+], function($, _, Backbone){
+  return {};
+});
